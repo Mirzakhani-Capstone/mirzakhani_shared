64a1df7904c23441213a28f2d913cc63b2010adf
Naomi Sweatt <sweatt.b.naomi@gmail.com>
2023-01-24 13:51:27 -0600

    features added

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+features = ['climateregions__climateregion', 'elevation__elevation', 'lat', 'lon', 'startdate',
+                 'contest-pevpr-sfc-gauss-14d__pevpr','contest-precip-14d__precip','contest-pres-sfc-gauss-14d__pres',
+                 'contest-prwtr-eatm-14d__prwtr','contest-rhum-sig995-14d__rhum','contest-slp-14d__slp',
+                 'contest-tmp2m-14d__tmp2m','contest-wind-h10-14d__wind-hgt-10','contest-wind-h100-14d__wind-hgt-100',
+                 'contest-wind-h500-14d__wind-hgt-500','contest-wind-h850-14d__wind-hgt-850','contest-wind-uwnd-250-14d__wind-uwnd-250',
+                 'contest-wind-uwnd-925-14d__wind-uwnd-925','contest-wind-vwnd-250-14d__wind-vwnd-250',
+                 'contest-wind-vwnd-925-14d__wind-vwnd-925']
 
 def get_explore_data():
     df = pd.read_csv('train_data.csv')
